1412a44a14fe794288681cad89fb002a9245e6f9
ohurjon <o3869006334@gmail.com>
2021-05-28 02:00:01 +0900

    Solve Problem!

--- src/main/java/kr/ohurjon/problem/no1011/Main1011.java
+++ src/main/java/kr/ohurjon/problem/no1011/Main1011.java
@@ -6,14 +6,33 @@ public class Main1011 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int A = sc.nextInt();
+        int T = sc.nextInt();
 
-        System.out.println(function(A));
+        int[] array = new int[T];
+
+        for (int i = 0; i < T; i++) {
+            array[i] = function(sc.nextInt(),sc.nextInt());
+        }
+
+        for (int arg: array) {
+            System.out.println(arg);
+        }
 
     }
 
-    static int function(int A){
-
-        return A;
+    static int function(int X,int Y){
+        int length = Y-X;
+        double sqrt = Math.sqrt(length);
+        int intsqrt = (int) sqrt;
+        if(intsqrt == sqrt){
+            return intsqrt*2-1;
+        }
+        if(Math.pow(intsqrt,2) + intsqrt >= length){
+            return intsqrt*2;
+        }
+        if(Math.pow(intsqrt+1,2) >= length){
+            return intsqrt*2+1;
+        }
+        return -1;
     }
 }
--- src/main/java/kr/ohurjon/problem/no8959/Main8958.java
+++ src/main/java/kr/ohurjon/problem/no8959/Main8958.java
@@ -1,4 +1,30 @@
 package kr.ohurjon.problem.no8959;
 
+import java.util.Scanner;
+
 public class Main8958 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int T = sc.nextInt();
+
+        int[] array = new int[T];
+
+        for (int i = 0; i < T; i++) {
+            array[i] = function(sc.next());
+        }
+
+        for (int arg: array) {
+            System.out.println(arg);
+        }
+
+    }
+
+    static int function(String Line){
+        int N = 0;
+        for( String A : Line.split("X")){
+            N += A.length()*(A.length()+1)/2;
+        }
+        return N;
+    }
 }
